8ba565a5f5581601406fa750d12defe3266aab95
josuemarcos <josue.fernandes@ee.ufcg.edu.br>
2025-10-09 16:51:40 -0300

    Add email and roles info to JWT token

--- src/main/java/com/example/lestock/config/AuthorizationServerConfiguration.java
+++ src/main/java/com/example/lestock/config/AuthorizationServerConfiguration.java
@@ -1,4 +1,5 @@
 package com.example.lestock.config;
+import com.example.lestock.security.CustomAuthentication;
 import com.nimbusds.jose.jwk.JWKSet;
 import com.nimbusds.jose.jwk.RSAKey;
 import com.nimbusds.jose.jwk.source.ImmutableJWKSet;
@@ -10,15 +11,19 @@ import org.springframework.core.annotation.Order;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.server.authorization.OAuth2TokenType;
 import org.springframework.security.oauth2.server.authorization.config.annotation.web.configuration.OAuth2AuthorizationServerConfiguration;
 import org.springframework.security.oauth2.server.authorization.config.annotation.web.configurers.OAuth2AuthorizationServerConfigurer;
 import org.springframework.security.oauth2.server.authorization.settings.AuthorizationServerSettings;
 import org.springframework.security.oauth2.server.authorization.settings.ClientSettings;
 import org.springframework.security.oauth2.server.authorization.settings.OAuth2TokenFormat;
 import org.springframework.security.oauth2.server.authorization.settings.TokenSettings;
+import org.springframework.security.oauth2.server.authorization.token.JwtEncodingContext;
+import org.springframework.security.oauth2.server.authorization.token.OAuth2TokenCustomizer;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.LoginUrlAuthenticationEntryPoint;
 import java.security.KeyPair;
@@ -26,6 +31,8 @@ import java.security.KeyPairGenerator;
 import java.security.interfaces.RSAPrivateKey;
 import java.security.interfaces.RSAPublicKey;
 import java.time.Duration;
+import java.util.Collection;
+import java.util.List;
 import java.util.UUID;
 
 @Configuration
@@ -107,5 +114,24 @@ public class AuthorizationServerConfiguration {
                 .jwkSetEndpoint("/oauth2/jwks")
                 .oidcLogoutEndpoint("/oauth2/logout")
                 .build();
+    }
+
+    @Bean
+    public OAuth2TokenCustomizer<JwtEncodingContext> tokenCustomizer() {
+        return context -> {
+            var principal = context.getPrincipal();
+
+            if (principal instanceof CustomAuthentication authentication) {
+                OAuth2TokenType tokenType = context.getTokenType();
+                Collection<? extends GrantedAuthority> authorities = authentication.getAuthorities();
+                List<String> authoritiesList = authorities.stream().map(GrantedAuthority::getAuthority).toList();
+
+                if(tokenType == OAuth2TokenType.ACCESS_TOKEN) {
+                    context.getClaims()
+                            .claim("authorities", authoritiesList)
+                            .claim("email", authentication.getUser().getEmail());
+                }
+            }
+        };
     }
 }
