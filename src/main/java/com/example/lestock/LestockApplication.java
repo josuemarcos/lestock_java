83988fecfdadfe686fd4ee632d461697ed8e510c
josuemarcos <josue.fernandes@ee.ufcg.edu.br>
2025-11-19 16:34:11 -0300

    Configure application to run with docker

--- src/main/java/com/example/lestock/LestockApplication.java
+++ src/main/java/com/example/lestock/LestockApplication.java
@@ -1,6 +1,4 @@
 package com.example.lestock;
-
-import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
@@ -10,8 +8,6 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 public class LestockApplication {
 
 	public static void main(String[] args) {
-        Dotenv dotenv = Dotenv.load();
-        dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
 		SpringApplication.run(LestockApplication.class, args);
 	}
 
