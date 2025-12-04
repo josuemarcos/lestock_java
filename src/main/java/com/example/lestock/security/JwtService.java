801d96ae4d0522da115d30e9717042bcf56db755
josuemarcos <josue.fernandes@ee.ufcg.edu.br>
2025-12-04 13:39:54 -0300

    Create JwtAuthenticationFilter

--- src/main/java/com/example/lestock/security/JwtService.java
+++ src/main/java/com/example/lestock/security/JwtService.java
@@ -1,10 +1,13 @@
 package com.example.lestock.security;
 
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
+
+import javax.crypto.SecretKey;
 import java.util.Date;
 
 @Service
@@ -20,5 +23,27 @@ public class JwtService {
                 .expiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60))
                 .signWith(Keys.hmacShaKeyFor(secret.getBytes()), Jwts.SIG.HS256)
                 .compact();
+    }
+
+    public String extractUsername(String token) {
+        return extractAllClaims(token).getSubject();
+    }
+
+    public boolean isTokenValid(String token, UserDetails  userDetails ) {
+        final String userName = extractUsername(token);
+        return (userName.equals(userDetails.getUsername()) && isTokenExpired(token));
+    }
+
+    private boolean isTokenExpired(String token) {
+        return extractAllClaims(token).getExpiration().before(new Date());
+    }
+
+    private Claims extractAllClaims(String token) {
+        SecretKey key = Keys.hmacShaKeyFor(secret.getBytes());
+        return Jwts.parser()
+                .verifyWith(key)
+                .build()
+                .parseSignedClaims(token)
+                .getPayload();
     }
 }
