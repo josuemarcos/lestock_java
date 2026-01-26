1a74a02f3848c66293cbfd4ee597f7a8e62148fd
josuemarcos <josue.fernandes@ee.ufcg.edu.br>
2026-01-26 08:55:11 -0300

    Remove client CRUD

--- src/main/java/com/example/lestock/dao/ClientDAO.java
+++
@@ -1,8 +0,0 @@
-package com.example.lestock.dao;
-import com.example.lestock.model.Client;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.Optional;
-
-public interface ClientDAO extends JpaRepository<Client, Long> {
-    Optional<Client> findByClientId(String clientId);
-}
